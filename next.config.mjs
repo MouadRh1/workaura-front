33e65ad508048fe089f60a8eadcb9a926f93a64f
Mouad Rahmouni <mouadrah01@gmail.com>
2026-06-12 17:10:02 +0100

    modification de reservation front

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  generateBuildId: async () => {
-    return 'build-' + Date.now()
-  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'saddlebrown-lion-880900.hostingersite.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'workaura.ma',
+        hostname: 'mintcream-mink-168421.hostingersite.com',
       },
     ],
   },
   reactStrictMode: true,
-  turbopack: {
-    root: './',
-  },
 }
 
 export default nextConfig
